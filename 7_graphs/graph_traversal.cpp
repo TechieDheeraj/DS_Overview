1935c1c4d0cf494da0097e42bc567552a19e6762
Dheeraj <Dheeraj.embdeveloper@gmail.com>
2020-04-27 16:09:04 +0530

    Implemented Graphh DFS

--- 7_graphs/graph_traversal.cpp
+++ 7_graphs/graph_traversal.cpp
@@ -1,18 +1,19 @@
 #include "7_graphAM.hpp"
 #include <queue>
+#include <stack>
 
 /* 
+   Finding BFS and DFS for graph in Adjacency Matrix
 
-   Finding BFS for graph in Adjacency Matrix
-
-   NOTE: BFS can be different on basis of starting Vertex
+   NOTE: BFS and DFS both can be different on basis of starting Vertex
 */
 
 class GraphTraversal {
    
    public:
       vector<int> visited;  // Creating vector of visited nodes
-      queue<int> queue; // Creating Queue for exploring adjacent nodes
+      queue<int> queue; // Creating Queue for exploring adjacent nodes in BFS
+      stack<int> stack; // Creating Queue for exploring adjacent nodes in DFS
 
       GraphTraversal(int vert) {
          visited.resize(vert);
@@ -21,6 +22,9 @@ class GraphTraversal {
       void graphBfs(int startIndex, GraphAM &gObj) { // Start Index should be in 0 - (vertices-1)
 
          int front = 0;
+         cout << "\n************** BFS **************** \n";
+
+         std::fill(visited.begin(), visited.end(), 0); // filling with zero(0) because visited array is also used in graphDfs()
 
          visited[startIndex] = 1;
          queue.push(startIndex);
@@ -41,6 +45,38 @@ class GraphTraversal {
                }
             }
          }
+         std::fill(visited.begin(), visited.end(), 0);
+         cout << "************************************\n";
+      }
+
+      void graphDfs(int index, GraphAM &gObj) {
+
+/*
+         int top = 0;
+
+         std::fill(visited.begin(), visited.end(), 0);
+
+         visited[startIndex] = 1;
+         stack.push(startIndex);
+
+         while(!stack.empty()) {
+
+            top = stack.top();
+            
+            gObj.gptr
+         }
+*/
+
+         if (visited[index] == 0) {
+
+            visited[index] = 1;
+            cout << "\nVisited: " << index << "\n";
+
+            for(int i=0; i < gObj.vert; ++i) {
+               if (((*gObj.gptr)[index][i] == 1) && (visited[i] == 0))
+                  graphDfs(i, gObj);
+            }
+         }
       }
 };
 
@@ -52,7 +88,7 @@ int main() {
    cin >> vertices;
 
    GraphAM gObj(vertices);
-   GraphBFS gBfs(vertices);
+   GraphTraversal gBfs(vertices);
 
    gObj.addEdge(0, 1);
    gObj.addEdge(0, 4);
@@ -64,6 +100,7 @@ int main() {
    gObj.printGraph();
 
    gBfs.graphBfs(0, gObj); // Find BFS of Graph
+   gBfs.graphDfs(0, gObj); // Find DFS of Graph
 
    return 0;
 }
